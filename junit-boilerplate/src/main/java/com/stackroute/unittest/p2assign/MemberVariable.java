354068536501a0c8ef4a63ee7e4c644289b47b6b
Kgharte <ghartekalpesh10@gmail.com>
2018-12-21 10:57:20 +0530

    Member Variable Test added

---
+++ junit-boilerplate/src/main/java/com/stackroute/unittest/p2assign/MemberVariable.java
@@ -0,0 +1,11 @@
+package com.stackroute.unittest.p2assign;
+
+public class MemberVariable {
+
+    public boolean display(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+        return true;
+    }
+}
